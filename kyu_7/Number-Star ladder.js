93ff51a455368bd0c6562b55586c7f9d57acb87d
EgorKirillov <80296253508@mail.ru>
2022-06-28 20:18:57 +0300

    Number-Star ladder 7

---
+++ kyu_7/Number-Star ladder.js
@@ -0,0 +1,45 @@
+/*
+Task
+Using n as a parameter in the function pattern, where n>0, complete the codes to get the pattern (take the help of examples):
+
+Note: There is no newline in the end (after the pattern ends)
+
+Examples
+pattern(3) should return "1\n1*2\n1**3", e.g. the following:
+
+    1
+1*2
+1**3
+pattern(10): should return the following:
+
+    1
+1*2
+1**3
+1***4
+1****5
+1*****6
+1******7
+1*******8
+1********9
+1*********10
+
+describe("Tests", () => {
+    it("test", () => {
+        Test.assertEquals(pattern(3),"1\n1*2\n1**3");
+        Test.assertEquals(pattern(7),"1\n1*2\n1**3\n1***4\n1****5\n1*****6\n1******7");
+        Test.assertEquals(pattern(20),"1\n1*2\n1**3\n1***4\n1****5\n1*****6\n1******7\n1*******8\n1********9\n1*********10\n1**********11\n1***********12\n1************13\n1*************14\n1**************15\n1***************16\n1****************17\n1*****************18\n1******************19\n1*******************20");
+    });
+});
+*/
+function pattern(n) {
+    let  output = "1";
+    for (let i = 2; i <= n; i++) {
+        output += "\n1"
+        for (let j = 1; j <= i; j++) {
+            output += (j === i)?j:"*"
+        }
+    }
+    return output;
+}
+
+console.log(pattern(10))
